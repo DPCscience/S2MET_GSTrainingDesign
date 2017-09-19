1aa45aaf83bea20735cb4b583cb48e7fa34b1e7a
neyhartj <neyhartje@gmail.com>
2017-09-19 09:43:51 -0500

    Added rescaling prior to predictions - this may help with svd convergence issues

--- Predictions/MSI/S2_MET_cluster_prediction.R
+++ Predictions/MSI/S2_MET_cluster_prediction.R
@@ -174,8 +174,12 @@ pred_acc_out <- function(train_df, test_df) {
   if (nrow(train_df) == 0 | nrow(test_df) == 0) {
     return(as.data.frame(NA))
   } else {
+    
+    # Rescale the data
+    train_df1 <- train_df %>% mutate(value = scale(value))
+    
     # Create model matrices
-    mf <- model.frame(value ~ line_name + environment, data = train_df)
+    mf <- model.frame(value ~ line_name + environment, data = train_df1)
     y <- model.response(mf)
     if (n_distinct(mf$environment) == 1) {
       X <- model.matrix(~ 1, data = mf)
@@ -193,7 +197,10 @@ pred_acc_out <- function(train_df, test_df) {
       filter(line_name %in% test_df$line_name)
     
     # Measure accuracy
-    full_join(test_pred, select(test_df, line_name, value)) %>% 
+    test_df %>%
+      mutate(line_name = as.character(line_name)) %>%
+      select(line_name, value) %>%
+      full_join(test_pred, ., by = "line_name") %>% 
       do({pred_acc = boot_cor(x = .$pred, y = .$value, boot.reps = 1000)})
   }
 }
@@ -244,6 +251,9 @@ ge_mean_D <- S2_MET_BLUE_entries_pop %>%
 
 
 
+
+
+
 # Extract the GxE BLUPs and run PCA
 # Extract the blups from the base_mod and then from the tp_mod
 ge_blup <- data_frame(
@@ -274,7 +284,8 @@ ge_blup_PCA <- ge_blup %>%
       apply(MARGIN = 2, FUN = function(env) ifelse(is.na(env), mean(env, na.rm = T), env)) %>% 
       prcomp() })) %>%
   mutate(PCs = list(prcomp$rotation)) %>%
-  mutate(PCA = list(dist(PCs[,1:n_PC])))
+  mutate(PCs_use = list(PCs[,1:n_PC])) %>%
+  mutate(PCA = list(dist(PCs_use)))
 
 ## Use the ge_means to perform factor analysis, extract the loadings
 ## matrix, then calculate the distance.
@@ -294,7 +305,7 @@ ge_mean_FA <- S2_MET_BLUE_entries_pop %>%
     ifelse(is.na(env), mean(env, na.rm = T), env)))) %>%
   mutate(fa_out = list(fa(r = mat, nfactors = 2))) %>%
   # Grab the loadings
-  mutate(delta = list(fa_out$loadings)) %>%
+  mutate(delta = list(structure(fa_out$loadings, class = "matrix"))) %>%
   # Distance matrix
   mutate(FA = list(dist(delta)))
 
@@ -306,8 +317,8 @@ EC_PCA <- as_data_frame(expand.grid(
   EC_multi = list(prcomp(t(multi_year_mat))$rotation[,1:n_PC])
 )) %>%
   rowwise() %>%
-  mutate(EC_one = list(dist(EC_one)),
-         EC_multi = list(dist(EC_multi)))
+  mutate(EC_one_dist = list(dist(EC_one)),
+         EC_multi_dist = list(dist(EC_multi)))
 
 
 
@@ -316,9 +327,19 @@ dist_df <- list(
   ge_mean_D,
   select(ge_blup_PCA, population, trait, PCA),
   select(ge_mean_FA, population, trait, FA),
-  EC_PCA
+  select(EC_PCA, population, trait, EC_one_dist, EC_multi_dist)
 ) %>%
   reduce(full_join)
+
+# # Create a data.frame with the matrices
+# mat_df <- list(
+#   select(ge_blup_PCA, population, trait, PCs_use),
+#   select(ge_mean_FA, population, trait, delta),
+#   select(EC_PCA, population, trait, EC_one, EC_multi)
+# ) %>%
+#   reduce(full_join)
+# 
+
 
 
 
@@ -329,8 +350,11 @@ clust_df <- dist_df %>%
   do(D_clust = hclust(.$D[[1]], method = "average"),
      PCA_clust = hclust(.$PCA[[1]], method = "average"),
      FA_clust = hclust(.$FA[[1]], method = "average"),
-     EC_one_clust = hclust(.$EC_one[[1]], method = "average"),
-     EC_multi_clust = hclust(.$EC_multi[[1]], method = "average"))
+     EC_one_clust = hclust(.$EC_one_dist[[1]], method = "average"),
+     EC_multi_clust = hclust(.$EC_multi_dist[[1]], method = "average"))
+
+# Set a maximum for the number of clusters
+max_K <- 10
 
 
 # For each trait and cluster strategy, determine the within and across cluster heritability
@@ -338,61 +362,63 @@ clust_df <- dist_df %>%
 clust_df_tomodel <- clust_df %>% 
   gather(method, clust, -trait, -population) %>% 
   # Add k's for clusters
-  by_row(function(i) seq(2, length(i$clust[[1]]$order) - 1), .to = "k") %>% 
+  by_row(function(i) seq(2, max_K), .to = "k") %>% 
   unnest(k, .drop = FALSE)
 
 
-
-
-# Iterate over clusters and calculate the heritabilities
-# Do this first for all entries in the population
-cluster_herit_all <- clust_df_tomodel %>%
-  filter(population == "all") %>%
-  by_row(function(i) {
-
-    # Cluster and cut the tree
-    clusters <- cutree(i$clust[[1]], k = i$k) %>%
-      data_frame(environment = names(.), cluster = .)
-
-    # Assign clusters to the dataset
-    S2_MET_tidy_use_clust <- left_join(S2_MET_tidy_use, clusters, "environment") %>%
-      mutate(cluster = as.factor(cluster)) %>%
-      filter(trait == i$trait) %>%
-      droplevels()
-
-    # Fit a new model with clusters
-    clust_mod <- lmer(value ~ (1|line) + environment + (1|line:cluster) +
-                        (1|line:environment) + (1|line:environment:cluster),
-                      data = S2_MET_tidy_use_clust)
-
-    clust_herit(clust_mod, g = "line", e = "environment", cl = "cluster") }, .to = "out_df")
-
-
-# Repeat for just the TP
-cluster_herit_tp <- clust_df_tomodel %>%
-  filter(population == "tp") %>%
-  by_row(function(i) {
-
-    # Cluster and cut the tree
-    clusters <- cutree(i$clust[[1]], k = i$k) %>%
-      data_frame(environment = names(.), cluster = .)
-
-    # Assign clusters to the dataset
-    S2_MET_tidy_use_clust <- left_join(S2_MET_tidy_use, clusters, "environment") %>%
-      mutate(cluster = as.factor(cluster)) %>%
-      filter(trait == i$trait, line_name %in% c(tp, checks)) %>%
-      droplevels()
-
-    # Fit a new model with clusters
-    clust_mod <- lmer(value ~ (1|line) + environment + (1|line:cluster) +
-                        (1|line:environment) + (1|line:environment:cluster),
-                      data = S2_MET_tidy_use_clust)
-
-    clust_herit(clust_mod, g = "line", e = "environment", cl = "cluster") }, .to = "out_df")
-
-# Save the data
-save_file <- file.path(pred_dir, "Results/cluster_heritability.RData")
-save("cluster_herit_all", "cluster_herit_tp", file = save_file)
+# 
+# 
+# # Iterate over clusters and calculate the heritabilities
+# # Do this first for all entries in the population
+# cluster_herit_all <- clust_df_tomodel %>%
+#   filter(population == "all") %>%
+#   group_by(population, trait, method, k) %>%
+#   do(out_df = {
+# 
+#     # Cluster and cut the tree
+#     clusters <- cutree(.$clust[[1]], k = .$k) %>%
+#       data_frame(environment = names(.), cluster = .)
+# 
+#     # Assign clusters to the dataset
+#     S2_MET_tidy_use_clust <- left_join(S2_MET_tidy_use, clusters, "environment") %>%
+#       mutate(cluster = as.factor(cluster)) %>%
+#       filter(trait == .$trait) %>%
+#       droplevels()
+# 
+#     # Fit a new model with clusters
+#     clust_mod <- lmer(value ~ (1|line) + environment + (1|line:cluster) +
+#                         (1|line:environment) + (1|line:environment:cluster),
+#                       data = S2_MET_tidy_use_clust)
+# 
+#     clust_herit(clust_mod, g = "line", e = "environment", cl = "cluster") })
+# 
+# 
+# # Repeat for just the TP
+# cluster_herit_tp <- clust_df_tomodel %>%
+#   filter(population == "tp") %>%
+#   group_by(population, trait, method, k) %>%
+#   do(out_df = {
+# 
+#     # Cluster and cut the tree
+#     clusters <- cutree(.$clust[[1]], k = .$k) %>%
+#       data_frame(environment = names(.), cluster = .)
+# 
+#     # Assign clusters to the dataset
+#     S2_MET_tidy_use_clust <- left_join(S2_MET_tidy_use, clusters, "environment") %>%
+#       mutate(cluster = as.factor(cluster)) %>%
+#       filter(trait == .$trait, line_name %in% c(tp, checks)) %>%
+#       droplevels()
+# 
+#     # Fit a new model with clusters
+#     clust_mod <- lmer(value ~ (1|line) + environment + (1|line:cluster) +
+#                         (1|line:environment) + (1|line:environment:cluster),
+#                       data = S2_MET_tidy_use_clust)
+# 
+#     clust_herit(clust_mod, g = "line", e = "environment", cl = "cluster") }, .to = "out_df")
+# 
+# # Save the data
+# save_file <- file.path(pred_dir, "Results/cluster_heritability.RData")
+# save("cluster_herit_all", "cluster_herit_tp", file = save_file)
 
 
 ## Prediction
@@ -400,8 +426,8 @@ save("cluster_herit_all", "cluster_herit_tp", file = save_file)
 # it.
 # Iterate over clusters
 # First do this using clusters created by all entries
-cluster_pred_acc_all <- clust_df_tomodel %>%
-  filter(population == "all", between(k, 4, 6)) %>%
+cluster_pred_acc_setup <- clust_df_tomodel %>%
+  filter(k == 5) %>%
   rename(tr = trait) %>%
   # Add core
   mutate(core = sort(rep(seq(1, n_cores), length.out = nrow(.)))) %>%
@@ -433,63 +459,46 @@ cluster_pred_acc_all <- clust_df_tomodel %>%
   # Remove unnecessary columns
   select(-clust_df, -phenos_cluster)
   
-
-cluster_pred_acc_all_results <- cluster_pred_acc_all %>%
-  unnest(pred_df) %>%
-  by_row(function(i) {
-    pred_acc_out(train_df = i$train_df[[1]], test_df = i$test_df[[1]])
-  }, .to = "acc") %>%
-  select(cluster, pred_env = cluster_env, acc)
-  
-
-        
-
-# Next do this using clusters created by only the tp entries
-# Remove the EC clustering strategies because these are identical to above
-cluster_pred_acc_tp <- clust_df_tomodel %>%
-  filter(population == "tp", between(k, 4, 6)) %>%
-  rename(tr = trait) %>%
-  # Add core
-  mutate(core = sort(rep(seq(1, n_cores), length.out = nrow(.)))) %>%
-  rowwise() %>%
-  
-  # Cut the tree and assign environments to clusters
-  mutate(clust_df = list({
-    cutree(clust, k = k) %>%
-      data_frame(cluster_env = names(.), cluster = .) %>%
-      mutate(cluster = paste("cluster", cluster, sep = "")) })) %>%
-  
-  # Add the clusters to the BLUEs data
-  mutate(phenos_cluster = list({
-    left_join(S2_MET_BLUE_entries, clust_df, c("environment" = "cluster_env")) %>%
-      mutate(cluster = as.factor(cluster)) %>%
-      filter(trait == as.character(tr)) %>%
-      droplevels() })) %>%
-  
-  # Create a df of the the training and testing data for each cluster/environmnent
-  mutate(pred_df = list({
-    clust_df %>% 
-      group_by(cluster, cluster_env) %>% 
-      do(data = filter(phenos_cluster, cluster == .$cluster)) %>%
-      mutate(
-        train_df = list(filter(data, environment != cluster_env, line_name %in% tp)), 
-        test_df = list(filter(data, environment == cluster_env, line_name %in% vp))) %>%
-      select(-data) })) %>%
-  
-  # Remove unnecessary columns
-  select(-clust_df, -phenos_cluster)
-
 # Run predictions
-cluster_pred_acc_tp_results <- cluster_pred_acc_tp %>%
-  unnest(pred_df) %>%
-  by_row(function(i) {
-    pred_acc_out(train_df = i$train_df[[1]], test_df = i$test_df[[1]])
-  }, .to = "acc") %>%
-  select(cluster, pred_env = cluster_env, acc)
+cluster_pred_acc <- cluster_pred_acc_setup %>%
+  unnest(pred_df) %>% 
+  group_by(population, tr, method, k, cluster, cluster_env) %>%
+  do(acc = pred_acc_out(train_df = .$train_df[[1]], test_df = .$test_df[[1]])) %>%
+  select(population, trait = tr, method, k, cluster, pred_env = cluster_env, acc)
+
+
+# Create a similar data.frame, but for predicting each environment (LOO) using all
+# other environments (non-clustered)
+nocluster_pred_acc_all <- distinct(S2_MET_BLUE_entries, trait, environment) %>%
+  rename(tr = trait, cluster_env = environment) %>% 
+  rowwise() %>%
+  mutate(cluster = "cluster0", method = "no_cluster") %>%
+  
+  # Add the BLUEs data
+  mutate(phenos_cluster = list({
+    S2_MET_BLUE_entries %>%
+      filter(trait == as.character(tr)) %>%
+      droplevels() })) %>%
+  group_by(tr, method, cluster_env, cluster) %>% 
+  do(train_df = {filter(.$phenos_cluster[[1]], environment != .$cluster_env[1], line_name %in% tp)}, 
+     test_df = {filter(.$phenos_cluster[[1]], environment == .$cluster_env[1], line_name %in% vp)})
+
+nocluster_pred_acc_all_results <- nocluster_pred_acc_all %>%
+  group_by(tr, method, cluster, cluster_env) %>%
+  do(acc = pred_acc_out(train_df = .$train_df[[1]], test_df = .$test_df[[1]])) %>%
+  select(trait = tr, method, cluster, pred_env = cluster_env, acc)
+
+
+
+# Combine
+pred_acc_results <- bind_rows(
+  cluster_pred_acc_all_results,
+  cluster_pred_acc_tp_results,
+  nocluster_pred_acc_all_results)
 
 # Save the data
 save_file <- file.path(pred_dir, "Results/cluster_predictions.RData")
-save("cluster_pred_acc_tp_results", "cluster_pred_acc_all_results", file = save_file)
+save("pred_acc_results", file = save_file)
  
 
 
