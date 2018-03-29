b286e3e5b40c37f81c6c4418cea818d4833d4709
neyhartj <neyhartje@gmail.com>
2018-03-29 15:10:23 -0500

    Edits to bash script

--- Scripts/Clustering/run_cluster.sh
+++ Scripts/Clustering/run_cluster.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 #PBS -l walltime=24:00:00,mem=24gb,nodes=1:ppn=16
-#PBS -N S2_MET_prediction_random_env_add
+#PBS -N S2_MET_cluster_heritability
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
 
 # Change the working directory
-cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/S2MET
+cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/S2MET/Scripts/Clustering
 
-module load R/3.4.0
+module load R/3.2.0_intel_mkl
 
-# Additive GEBLUP predictions
-#Rscript Predictions/MSI/aGEBLUP_predictions.R
-
-# Interation GEBLUP predictions
-#Rscript Predictions/MSI/iGEBLUP_predictions.R
-
-# Clustering and prediction
-Rscript Predictions/S2MET_cluster_prediction.R
+# Cluster heritability calculations
+Rscript cluster_heritability.R
