aad65e3db1ddb6e85c96594840748943986bfe7a
neyhartj <neyha001@umn.edu>
2018-04-05 11:50:51 -0500

    edits to scripts and source

--- source_MSI.R
+++ source_MSI.R
@@ -4,7 +4,7 @@
 
 
 # Load packages
-packages <- c("dplyr", "tidyr", "stringr", "readxl", "readr", "parallel",
+packages <- c("dplyr", "tidyr", "tibble", "stringr", "readxl", "readr", "parallel",
               "rrBLUP", "purrr")
 package_dir <- "/panfs/roc/groups/6/smithkp/neyha001/R/x86_64-unknown-linux-gnu-library/3.2/"
 invisible(lapply(packages, library, character.only = TRUE, lib.loc = package_dir))
