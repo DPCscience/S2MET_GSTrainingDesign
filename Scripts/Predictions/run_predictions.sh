028ae0744cf70d882337d4252e4f22fcb4a77399
neyhartj <neyha001@umn.edu>
2018-04-07 16:21:38 -0500

    Edits to distance prediction/heritability scripts

--- Scripts/Predictions/run_predictions.sh
+++ Scripts/Predictions/run_predictions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -l walltime=24:00:00,mem=24gb,nodes=1:ppn=8
-#PBS -N S2_MET_prediction_environmental_distance
+#PBS -N S2_MET_heritability_environmental_distance
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
@@ -12,4 +12,7 @@ cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/S2MET/Scripts/Predicti
 module load R/3.2.0_intel_mkl
 
 # Predictions by environmental rank
-Rscript distance_rank_predictions.R
+#Rscript distance_rank_predictions.R
+
+# Heritability by environmental rank
+Rscript distance_rank_heritability.R
