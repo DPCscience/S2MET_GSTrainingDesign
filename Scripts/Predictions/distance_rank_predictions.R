46e54547c8cfbd69e37c6128918624b42eb81ba6
neyhartj <neyhartje@gmail.com>
2018-04-12 09:04:49 -0500

    Commented the cumulative prediction and un-commented the window predictions

--- Scripts/Predictions/distance_rank_predictions.R
+++ Scripts/Predictions/distance_rank_predictions.R
@@ -79,8 +79,90 @@ pred_env_dist_rank_split <- pred_env_dist_rank %>%
   assign_cores(n_core = n_core) %>%
   split(.$core)
 
+# # Run over multiple cores
+# env_dist_predictions_out <- mclapply(X = pred_env_dist_rank_split, FUN = function(core_df) {
+# 
+#   ## Create an empty results list
+#   results_out <- vector("list", nrow(core_df))
+# 
+#   # Iterate over the rows in the core_df
+#   for (i in seq_along(results_out)) {
+# 
+#     ## Given an ordered list of environments sorted by distance to the prediction
+#     ## environment, implement a function that calculates a cumulative mean distance
+#     ## and then use the information from those environments to run predictions
+#     pred_env <- core_df$environment[i]
+#     tr <- core_df$trait[i]
+#     dist_met <- core_df$dist_method[i]
+# 
+#     # Subset the trainin environments for the trait
+#     tr_train_env <- train_envs_traits %>% filter(trait == tr) %>% pull(environment)
+# 
+#     # Subset the prediction environment data from the BLUEs
+#     pred_env_data <- S2_MET_BLUEs_use %>%
+#       filter(environment == pred_env, trait == tr, line_name %in% vp_geno)
+# 
+#     # Remove the environments not in the training environments for that trait
+#     sorted_train_envs <- core_df$env_rank[[i]] %>% unlist() %>% .[names(.) %in% tr_train_env]
+# 
+#     ## Use the accumulate function in dplyr to create lists of training environments
+#     ## and the cumulative mean distance from the prediction environment
+#     train_envs_accumulate <- sorted_train_envs %>%
+#       names() %>%
+#       accumulate(~c(.x, .y)) %>%
+#       map(~sorted_train_envs[.]) %>%
+#       map(~list(train_envs = names(.), cummean_dist = mean(.)))
+# 
+#     # Map over these environments and gather training data
+#     train_envs_data <- train_envs_accumulate %>%
+#       map(~filter(S2_MET_BLUEs_use, environment %in% .$train_envs, trait == tr, line_name %in% tp_geno))
+# 
+#     # Map over the data and predict
+#     predictions_out <- train_envs_data %>%
+#       map(~rename(., env = environment)) %>%
+#       map(~gblup(K = K, train = ., test = pred_env_data, bootreps = 100))
+# 
+#     # Return the bootstrap data.frame results
+#     predictions_boot <- predictions_out %>%
+#       map_df("boot") %>%
+#       rename(accuracy = cor)
+# 
+#     # Return a data_frame with the training environments, cumumative mean distance,
+#     # and the prediction accuracy results
+#     results_out[[i]] <- bind_cols(
+#       data_frame(train_envs = map(train_envs_accumulate, "train_envs"),
+#                  distance = map_dbl(train_envs_accumulate, "cummean_dist")),
+#       predictions_boot)
+# 
+#   } # Close the for loop
+# 
+#   # Add the results list to the original core DF
+#   core_df %>%
+#     select(-env_rank, -core) %>%
+#     mutate(results_out = results_out)
+# 
+# }, mc.cores = n_core)
+# 
+# 
+# # Save the results
+# save_file <- file.path(result_dir, "environmental_distance_predictions.RData")
+# save("env_dist_predictions_out", file = save_file)
+
+
+
+
+
+### Use the distance objects to rank training environments relevative to prediction 
+### environments. Then use a sliding window of k environments to create training
+### sets to predict the 'prediction environment'
+### 
+
+# Number of environments in the sliding window
+window_size <- 5
+
+
 # Run over multiple cores
-env_dist_predictions_out <- mclapply(X = pred_env_dist_rank_split, FUN = function(core_df) {
+env_dist_window_predictions_out <- mclapply(X = pred_env_dist_rank_split, FUN = function(core_df) {
 
   ## Create an empty results list
   results_out <- vector("list", nrow(core_df))
@@ -105,17 +187,21 @@ env_dist_predictions_out <- mclapply(X = pred_env_dist_rank_split, FUN = functio
     # Remove the environments not in the training environments for that trait
     sorted_train_envs <- core_df$env_rank[[i]] %>% unlist() %>% .[names(.) %in% tr_train_env]
 
-    ## Use the accumulate function in dplyr to create lists of training environments
-    ## and the cumulative mean distance from the prediction environment
-    train_envs_accumulate <- sorted_train_envs %>%
-      names() %>%
-      accumulate(~c(.x, .y)) %>%
+    # Create a list of length (n - k + 1) of the training environments in the sliding
+    # window
+    n <- length(sorted_train_envs) - window_size
+
+    # Create the list
+    train_envs_accumulate <- seq(0, n) %>%
+      map(~. + seq(window_size)) %>%
       map(~sorted_train_envs[.]) %>%
-      map(~list(train_envs = names(.), cummean_dist = mean(.)))
+      map(~list(train_envs = names(.), mean_dist = mean(.)))
+
 
     # Map over these environments and gather training data
     train_envs_data <- train_envs_accumulate %>%
       map(~filter(S2_MET_BLUEs_use, environment %in% .$train_envs, trait == tr, line_name %in% tp_geno))
+
 
     # Map over the data and predict
     predictions_out <- train_envs_data %>%
@@ -131,7 +217,7 @@ env_dist_predictions_out <- mclapply(X = pred_env_dist_rank_split, FUN = functio
     # and the prediction accuracy results
     results_out[[i]] <- bind_cols(
       data_frame(train_envs = map(train_envs_accumulate, "train_envs"),
-                 distance = map_dbl(train_envs_accumulate, "cummean_dist")),
+                 distance = map_dbl(train_envs_accumulate, "mean_dist")),
       predictions_boot)
 
   } # Close the for loop
@@ -144,96 +230,10 @@ env_dist_predictions_out <- mclapply(X = pred_env_dist_rank_split, FUN = functio
 }, mc.cores = n_core)
 
 
+
 # Save the results
-save_file <- file.path(result_dir, "environmental_distance_predictions.RData")
-save("env_dist_predictions_out", file = save_file)
-
-
-
-
-
-### Use the distance objects to rank training environments relevative to prediction 
-### environments. Then use a sliding window of k environments to create training
-### sets to predict the 'prediction environment'
-### 
-
-# # Number of environments in the sliding window
-# window_size <- 5
-# 
-# 
-# # Run over multiple cores
-# env_dist_window_predictions_out <- mclapply(X = pred_env_dist_rank_split, FUN = function(core_df) {
-#   
-#   ## Create an empty results list
-#   results_out <- vector("list", nrow(core_df))
-#   
-#   # Iterate over the rows in the core_df
-#   for (i in seq_along(results_out)) {
-#     
-#     ## Given an ordered list of environments sorted by distance to the prediction
-#     ## environment, implement a function that calculates a cumulative mean distance
-#     ## and then use the information from those environments to run predictions
-#     pred_env <- core_df$environment[i]
-#     tr <- core_df$trait[i]
-#     dist_met <- core_df$dist_method[i]
-#     
-#     # Subset the trainin environments for the trait
-#     tr_train_env <- train_envs_traits %>% filter(trait == tr) %>% pull(environment)
-#     
-#     # Subset the prediction environment data from the BLUEs
-#     pred_env_data <- S2_MET_BLUEs_use %>%
-#       filter(environment == pred_env, trait == tr, line_name %in% vp_geno)
-#     
-#     # Remove the environments not in the training environments for that trait
-#     sorted_train_envs <- core_df$env_rank[[i]] %>% unlist() %>% .[names(.) %in% tr_train_env]
-#     
-#     # Create a list of length (n - k + 1) of the training environments in the sliding
-#     # window
-#     n <- length(sorted_train_envs) - window_size
-#     
-#     # Create the list
-#     train_envs_accumulate <- seq(0, n) %>% 
-#       map(~. + seq(window_size)) %>%
-#       map(~sorted_train_envs[.]) %>%
-#       map(~list(train_envs = names(.), mean_dist = mean(.)))
-# 
-#     
-#     # Map over these environments and gather training data
-#     train_envs_data <- train_envs_accumulate %>%
-#       map(~filter(S2_MET_BLUEs_use, environment %in% .$train_envs, trait == tr, line_name %in% tp_geno))
-#     
-#     
-#     # Map over the data and predict
-#     predictions_out <- train_envs_data %>%
-#       map(~rename(., env = environment)) %>%
-#       map(~gblup(K = K, train = ., test = pred_env_data, bootreps = 100))
-#     
-#     # Return the bootstrap data.frame results
-#     predictions_boot <- predictions_out %>%
-#       map_df("boot") %>%
-#       rename(accuracy = cor)
-#     
-#     # Return a data_frame with the training environments, cumumative mean distance,
-#     # and the prediction accuracy results
-#     results_out[[i]] <- bind_cols(
-#       data_frame(train_envs = map(train_envs_accumulate, "train_envs"),
-#                  distance = map_dbl(train_envs_accumulate, "mean_dist")),
-#       predictions_boot)
-#     
-#   } # Close the for loop
-#   
-#   # Add the results list to the original core DF
-#   core_df %>%
-#     select(-env_rank, -core) %>%
-#     mutate(results_out = results_out)
-#   
-# }, mc.cores = n_core)
-# 
-# 
-# 
-# # Save the results
-# save_file <- file.path(result_dir, "environmental_distance_window_predictions.RData")
-# save("env_dist_window_predictions_out", file = save_file)
+save_file <- file.path(result_dir, "environmental_distance_window_predictions.RData")
+save("env_dist_window_predictions_out", file = save_file)
 
 
 
--- Scripts/Predictions/distance_rank_predictions_analysis.R
+++ Scripts/Predictions/distance_rank_predictions_analysis.R
@@ -1,7 +1,7 @@
 ## Analysis of predictions based on environmental distance
 ## 
 ## Author: Jeff Neyhart
-## Last Updated: April 9, 2018
+## Last Updated: April 11, 2018
 ## 
 ## This script will look at prediction accuracies from adding environments after
 ## ranking based on specific distance metrics
@@ -20,8 +20,6 @@ load(file.path(result_dir, "environmental_distance_predictions.RData"))
 load(file.path(result_dir, "environmental_distance_window_predictions.RData"))
 load(file.path(result_dir, "environmental_distance_heritability.RData"))
 
-# Number of environments per page
-nenv_pg <- 8
 
 
 
@@ -33,11 +31,69 @@ cumulative_pred_results <- env_dist_predictions_out %>%
 
 # Re-scale the distance measurements to unit variance
 cumulative_pred_toplot <- cumulative_pred_results %>%
+  # Remove the environmental correlation method
+  filter(dist_method %in% names(dist_method_replace)) %>%
   group_by(environment, trait, dist_method) %>% 
   mutate(scaled_distance = scale(distance),
          scaled_distance = scaled_distance - min(scaled_distance)) %>%
   ungroup() %>%
-  mutate(dist_method = str_replace_all(dist_method, dist_method_replace))
+  mutate(dist_method = str_replace_all(dist_method, dist_method_replace),
+         n_train_env = map_dbl(train_envs, length))
+
+
+
+
+
+## For each prediction environment, trait, and distance method, find the prediction
+## accuracy after adding all environments (i.e. the last value) and the local
+## maximum prediction accuracy (if present)
+## 
+cumulative_pred_toplot_maxima <- cumulative_pred_toplot %>% 
+  group_by(environment, trait, dist_method) %>% 
+  mutate(local_max_accuracy = max(accuracy),
+         local_max_n_train = n_train_env[accuracy == local_max_accuracy],
+         local_max_scaled_dist = scaled_distance[accuracy == local_max_accuracy],
+         all_env_acccuracy = accuracy[n_train_env == max(n_train_env)]) %>%
+  ungroup() %>%
+  select(-train_envs) %>%
+  # Is the local maximum greater than the final accuracy?
+  # What is the difference between the local maximum and the final
+  mutate(local_adv = local_max_accuracy - all_env_acccuracy,
+         local_better = (local_adv > 0))
+
+## Filter for the observations in which the accuracy is the local maximum
+cumulative_pred_toplot_maxima_filter <- cumulative_pred_toplot_maxima %>%
+  filter(accuracy == local_max_accuracy)
+  
+
+
+
+## Distribution of the number of training environments or the scaled distance (cumulative) 
+## of the local maximum
+g_local_max <- cumulative_pred_toplot_maxima_filter %>% 
+  ggplot(aes(x = n_train_env, fill = dist_method)) +
+  # ggplot(aes(x = scaled_distance, fill = dist_method)) + 
+  geom_density(alpha = 0.25) + 
+  facet_wrap(~ trait, ncol = 2) +
+  theme_bw()
+
+## Distribution of local advantage
+g_local_adv <- cumulative_pred_toplot_maxima_filter %>% 
+  ggplot(aes(x = local_adv, fill = dist_method)) + 
+  geom_density(alpha = 0.25) + 
+  facet_wrap(~ trait, ncol = 2) +
+  theme_bw()
+
+
+## Calculate the average environmental distance (scaled) where the local maxima is
+## found
+cumulative_pred_maxima_summary <- cumulative_pred_toplot_maxima_filter %>% 
+  group_by(trait, dist_method) %>% 
+  summarize_at(vars(scaled_distance, n_train_env, local_adv, local_better), mean)
+
+
+
+
   
 
 # ## Sample environments that have all traits
@@ -47,50 +103,40 @@ cumulative_pred_toplot <- cumulative_pred_results %>%
 
 ## Common plot modifier
 g_mod <- list(
-  geom_point(),
-  geom_line(),
+  geom_point(aes(x = local_max_scaled_dist, y = local_max_accuracy)),
+  geom_line(lwd = 0.5),
   # geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper)),
   ylab("Prediction Accuracy"),
   xlab("Cumulative Distance of Environments in Training Set"),
   theme_bw(),
   theme(panel.grid = element_blank(),
-        legend.key.height = unit(2, units = "line")),
+        legend.key.height = unit(2, units = "line"),
+        text = element_text(size = 8)),
   labs(title = "Cumulative Environmental Clusters")
 )
 
 # List over traits
-g_cumulative_pred <- traits %>%
+g_cumulative_pred <- setNames(traits, traits) %>%
   map(function(tr) {
     # Subset the trait
-    cumulative_pred_toplot_tr <- cumulative_pred_toplot %>%
+    cumulative_pred_toplot_tr <- cumulative_pred_toplot_maxima %>%
       filter(trait == tr) %>%
       mutate(environment = factor(environment, levels = levels(env_herit_rank[[tr]]$environment)))
     
-    # Calculate the number of pages
-    ngrp <- n_distinct(cumulative_pred_toplot_tr$environment)
-    npgs <- ngrp / nenv_pg
+    # Create one plot using facet_wrap
+    g_total <- cumulative_pred_toplot_tr %>% 
+      ggplot(aes(x = scaled_distance, y = accuracy, color = dist_method)) +
+      g_mod +
+      facet_wrap( ~ environment + trait) +
+      geom_ribbon(aes(ymin = ci_lower, ymax = ci_upper, fill = dist_method, color = NULL), alpha = 0.1)
     
-    # Round up
-    npgs_round <- ifelse(sign(round(npgs) - npgs) == 1, round(npgs), round(npgs + 1))
-    
-    # Iterate over the number of pages
-    g_pages <- map(seq(npgs_round), function(pg) {
-      cumulative_pred_toplot_tr %>% 
-        ggplot(aes(x = scaled_distance, y = accuracy, color = dist_method)) +
-        g_mod +
-        facet_grid_paginate(environment + trait ~ ., scales = "free_y", ncol = 1, 
-                            nrow = nenv_pg, page = pg)
-    })
-    
-    # Iterate over pages and save
-    for (i in seq_along(g_pages)) {
-      # Save
-      save_file <- file.path(fig_dir, str_c("cumulative_env_dist_pred_", tr, "_page", i, ".jpg"))
-      ggsave(filename = save_file, plot = g_pages[[i]], height = 10, width = 8, dpi = 1000)
-    }
+    # Save
+    save_file <- file.path(fig_dir, str_c("cumulative_env_dist_pred_", tr, ".jpg"))
+    ggsave(filename = save_file, plot = g_total, height = 15, width = 12, dpi = 1000)
     
     # Return the plot
-    return(g_pages)
+    # return(g_pages)
+    return(g_total)
     
   })
 
@@ -100,15 +146,30 @@ g_cumulative_pred <- traits %>%
 g_cumulative_pred1 <- dist_method_replace %>%
   map(function(dm) {
     
-    # Subset the trait and the distance method
-    g_list <- cumulative_pred_toplot %>% 
-      filter(dist_method == dm) %>% 
-      split(.$trait) %>% 
-      list(., env_herit_rank) %>% 
-      pmap(~mutate(.x, environment = factor(environment, levels = levels(.y$environment)))) %>%
-      map(~ggplot(., aes(x = scaled_distance, y = accuracy, color = environment)) +
-            g_mod +
-            facet_grid(trait + dist_method ~ .) )
+    # Subset the distance method
+    cumulative_pred_toplot_dm <- cumulative_pred_toplot_maxima %>%
+      filter(dist_method == dm)
+    
+    # Iterate over the trait
+    g_list <- setNames(traits, traits) %>%
+      map(function(tr) {
+        # Subset the trait data
+        cumulative_pred_toplot_tr <- cumulative_pred_toplot_dm %>%
+          filter(trait == tr) %>%
+          mutate(environment = factor(environment, levels = levels(env_herit_rank[[tr]]$environment)))
+        
+        # Extract the ylim values from the single plots created above
+        ylim_tr <- ggplot_build(g_cumulative_pred[[tr]])$layout$panel_ranges[[1]]$y.range
+        
+        # Create the plot and return
+        cumulative_pred_toplot_tr %>% 
+          ggplot(aes(x = scaled_distance, y = accuracy)) +
+          g_mod + 
+          ylim(ylim_tr) +
+          facet_wrap( ~ environment + trait + dist_method)  +
+          geom_ribbon(aes(ymin = ci_lower, ymax = ci_upper), alpha = 0.1)
+        
+      })
       
     # Iterate over the plots and save
     for (i in seq_along(g_list)) {
@@ -116,13 +177,21 @@ g_cumulative_pred1 <- dist_method_replace %>%
       save_file <- file.path(
         fig_dir, str_c("cumulative_env_dist_pred_", names(dist_method_replace[dist_method_replace == dm]), 
                        "_", names(g_list)[i], ".jpg"))
-      ggsave(filename = save_file, plot = g_list[[i]], height = 8, width = 8, dpi = 1000)
+      ggsave(filename = save_file, plot = g_list[[i]], height = 15, width = 12, dpi = 1000)
     }
     
     # Return the plot
     return(g_list)
     
   })
+
+
+
+
+
+
+
+
 
 
 
@@ -135,68 +204,135 @@ window_pred_results <- env_dist_window_predictions_out %>%
 
 # Re-scale the distance measurements to unit variance
 window_pred_toplot <- window_pred_results %>%
+  # Remove the environmental correlation method
+  filter(dist_method %in% names(dist_method_replace)) %>%
   group_by(environment, trait, dist_method) %>% 
   mutate(scaled_distance = scale(distance),
          scaled_distance = scaled_distance - min(scaled_distance)) %>%
   ungroup() %>%
-  mutate(dist_method = str_replace_all(dist_method, dist_method_replace))
+  mutate(dist_method = str_replace_all(dist_method, dist_method_replace),
+         n_train_env = map_dbl(train_envs, length))
 
 
-# ## Sample the same environments as those in the cumulative example above
-# window_pred_toplot_sample <- window_pred_toplot %>%
-#   filter(environment %in% unique(cumulative_pred_toplot_sample$environment))
+## Identify the largest prediction accuracy among the sliding windows
+window_pred_toplot_maxima <- window_pred_toplot %>%
+  group_by(environment, trait, dist_method) %>% 
+  mutate(local_max_accuracy = max(accuracy),
+         local_max_scaled_dist = scaled_distance[accuracy == local_max_accuracy]) %>%
+  ungroup() %>%
+  select(-train_envs)
+
+## Filter for the local accuracy maxima
+window_pred_toplot_maxima_filter <- window_pred_toplot_maxima %>% 
+  filter(accuracy == local_max_accuracy)
+  
 
 
-# Plot
+## Distribution of the number of training environments or the scaled distance (cumulative) 
+## of the local maximum
+g_local_max <- window_pred_toplot_maxima_filter %>% 
+  ggplot(aes(x = scaled_distance, fill = dist_method)) +
+  geom_density(alpha = 0.25) + 
+  facet_wrap(~ trait, ncol = 2) +
+  theme_bw() +
+  theme(legend.key.height = unit(2, "lines"))
+
+
+
+## Calculate the average environmental distance (scaled) where the local maxima is
+## found
+window_pred_maxima_summary <- window_pred_toplot_maxima_filter %>% 
+  group_by(trait, dist_method) %>% 
+  summarize_at(vars(scaled_distance), mean)
+
+
+
+
+## Common plot modifier
 g_mod <- list(
-  geom_point(),
-  geom_line(),
-  # geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper)), 
+  geom_point(aes(x = local_max_scaled_dist, y = local_max_accuracy)),
+  geom_line(lwd = 0.5),
+  # geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper)),
   ylab("Prediction Accuracy"),
   xlab("Mean Distance of Environments in the Training Set"),
   theme_bw(),
   theme(panel.grid = element_blank(),
-        legend.key.height = unit(2, units = "line")),
+        legend.key.height = unit(2, units = "line"),
+        text = element_text(size = 8)),
   labs(title = "Sliding Window Environmental Clusters")
 )
 
 
+
 # List over traits
-g_window_pred <- traits %>%
+g_window_pred <- setNames(traits, traits) %>%
   map(function(tr) {
     # Subset the trait
-    window_pred_toplot_tr <- window_pred_toplot %>%
-      filter(trait == tr)
+    window_pred_toplot_tr <- window_pred_toplot_maxima %>%
+      filter(trait == tr) %>%
+      mutate(environment = factor(environment, levels = levels(env_herit_rank[[tr]]$environment)))
     
-    # Calculate the number of pages
-    ngrp <- n_distinct(window_pred_toplot_tr$environment)
-    npgs <- ngrp / nenv_pg
+    # Create one plot using facet_wrap
+    g_total <- window_pred_toplot_tr %>% 
+      ggplot(aes(x = scaled_distance, y = accuracy, color = dist_method)) +
+      g_mod +
+      facet_wrap( ~ environment + trait) +
+      geom_ribbon(aes(ymin = ci_lower, ymax = ci_upper, fill = dist_method, color = NULL), alpha = 0.1)
     
-    # Round up
-    npgs_round <- ifelse(sign(round(npgs) - npgs) == 1, round(npgs), round(npgs + 1))
-    
-    # Iterate over the number of pages
-    g_pages <- map(seq(npgs_round), function(pg) {
-      window_pred_toplot_tr %>% 
-        ggplot(aes(x = scaled_distance, y = accuracy, color = dist_method)) +
-        g_mod +
-        facet_grid_paginate(environment + trait ~ ., scales = "free_y", ncol = 1, 
-                            nrow = nenv_pg, page = pg)
-    })
-    
-    # Iterate over pages and save
-    for (i in seq_along(g_pages)) {
-      # Save
-      save_file <- file.path(fig_dir, str_c("window_env_dist_pred_", tr, "_page", i, ".jpg"))
-      ggsave(filename = save_file, plot = g_pages[[i]], height = 10, width = 8, dpi = 1000)
-    }
+    # Save
+    save_file <- file.path(fig_dir, str_c("window_env_dist_pred_", tr, ".jpg"))
+    ggsave(filename = save_file, plot = g_total, height = 15, width = 12, dpi = 1000)
     
     # Return the plot
-    return(g_pages)
+    # return(g_pages)
+    return(g_total)
     
   })
 
 
+### Plot a single distance method over many environments
+# List over traits
+g_window_pred1 <- dist_method_replace %>%
+  map(function(dm) {
+    
+    # Subset the distance method
+    window_pred_toplot_dm <- window_pred_toplot_maxima %>%
+      filter(dist_method == dm)
+    
+    # Iterate over the trait
+    g_list <- setNames(traits, traits) %>%
+      map(function(tr) {
+        # Subset the trait data
+        window_pred_toplot_tr <- window_pred_toplot_dm %>%
+          filter(trait == tr) %>%
+          mutate(environment = factor(environment, levels = levels(env_herit_rank[[tr]]$environment)))
+        
+        # Extract the ylim values from the single plots created above
+        ylim_tr <- ggplot_build(g_window_pred[[tr]])$layout$panel_ranges[[1]]$y.range
+        
+        # Create the plot and return
+        window_pred_toplot_tr %>% 
+          ggplot(aes(x = scaled_distance, y = accuracy)) +
+          g_mod + 
+          ylim(ylim_tr) +
+          facet_wrap( ~ environment + trait + dist_method)  +
+          geom_ribbon(aes(ymin = ci_lower, ymax = ci_upper), alpha = 0.1)
+        
+      })
+    
+    # Iterate over the plots and save
+    for (i in seq_along(g_list)) {
+      # Save
+      save_file <- file.path(
+        fig_dir, str_c("window_env_dist_pred_", names(dist_method_replace[dist_method_replace == dm]), 
+                       "_", names(g_list)[i], ".jpg"))
+      ggsave(filename = save_file, plot = g_list[[i]], height = 15, width = 12, dpi = 1000)
+    }
+    
+    # Return the plot
+    return(g_list)
+    
+  })
 
 
 
