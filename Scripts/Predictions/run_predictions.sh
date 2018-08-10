1a453249e6a7e12727d83e16ec83f661b44b5d62
neyhartj <neyha001@umn.edu>
2018-08-09 20:35:23 -0500

    changes to batch script

--- Scripts/Predictions/run_predictions.sh
+++ Scripts/Predictions/run_predictions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # #PBS -l walltime=24:00:00,mem=62gb,nodes=1:ppn=24
-#PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=16
+#PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=8
 # #PBS -N S2_MET_environmental_distance_pred
 # #PBS -N S2_MET_environmental_distance_realistic
-#PBS -N S2_MET_environmental_distance_pred
+#PBS -N S2_MET_environmental_distance_pred_testing
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
@@ -15,7 +15,10 @@ cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/S2MET/Scripts/Predicti
 module load R/3.2.0_intel_mkl
 
 # Predictions by environmental rank
-Rscript distance_rank_predictions.R
+# Rscript distance_rank_predictions.R
+
+# TESTING
+Rscript distance_rank_predictions_TESTING.R
 
 # Heritability by environmental rank
 # Rscript distance_rank_heritability.R
