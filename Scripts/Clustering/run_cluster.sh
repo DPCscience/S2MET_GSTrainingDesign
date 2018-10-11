4eb64dab9717339bf301e76b44cb350f65221c63
neyhartj <neyha001@umn.edu>
2018-10-11 13:47:55 -0500

    bash edits

--- Scripts/Clustering/run_cluster.sh
+++ Scripts/Clustering/run_cluster.sh
@@ -2,7 +2,6 @@
 
 # #PBS -l walltime=36:00:00,mem=62gb,nodes=1:ppn=8
 #PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=8
-# #PBS -N distance_heritability
 #PBS -N distance_heritability
 #PBS -M neyha001@umn.edu
 #PBS -m abe
