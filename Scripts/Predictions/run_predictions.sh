915a338da3426b73540fd9a65cafb395bedc51b5
neyhartj <neyha001@umn.edu>
2018-10-23 12:36:15 -0500

    edits

--- Scripts/Predictions/run_predictions.sh
+++ Scripts/Predictions/run_predictions.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# #PBS -l walltime=24:00:00,mem=62gb,nodes=1:ppn=16
-#PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=8
+#PBS -l walltime=24:00:00,mem=62gb,nodes=1:ppn=16
+# #PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=8
 #PBS -N environmental_distance_pred
 # #PBS -N environmental_covariance_predictions
+# #PBS -N cluster_predictions
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
@@ -14,11 +15,11 @@ cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/S2MET/Scripts/Predicti
 module load R/3.5.0
 
 # Predictions by environmental rank
-# Rscript distance_rank_predictions.R
+Rscript distance_rank_predictions.R
 
 # Predictions 
 # Rscript environment_covariance_matrix_predictions.R
 
 # Cluster predictions
-Rscript cluster_predictions.R
+# Rscript cluster_predictions.R
 
