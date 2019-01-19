4a462b6b077c151ed3ded873537716eb41940baf
neyhartj <neyha001@umn.edu>
2019-01-19 14:52:09 -0600

    bash

--- Scripts/Predictions/run_predictions.sh
+++ Scripts/Predictions/run_predictions.sh
@@ -2,16 +2,17 @@
 
 # #PBS -l walltime=24:00:00,mem=62gb,nodes=1:ppn=16
 # #PBS -l walltime=24:00:00,mem=24gb,nodes=1:ppn=24
-#PBS -l walltime=04:00:00,mem=24gb,nodes=1:ppn=24
+#PBS -l walltime=24:00:00,mem=48gb,nodes=1:ppn=16
 # #PBS -N environmental_distance_pred
 # #PBS -N environmental_covariance_predictions
-#PBS -N cluster_preds
+# #PBS -N cluster_preds
+#PBS -N cross-validation
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
 
 # Change the working directory
-cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/S2MET/Scripts/Predictions/
+cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/S2MET_Predictions/Scripts/Predictions/
 
 module load R/3.5.0
 
