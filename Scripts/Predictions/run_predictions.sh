f0a6686f50b4b53f386d936dfbff003985ab50d5
neyhartj <neyha001@umn.edu>
2019-03-20 08:51:39 -0500

    add package to source

--- Scripts/Predictions/run_predictions.sh
+++ Scripts/Predictions/run_predictions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l walltime=24:00:00,mem=48gb,nodes=1:ppn=16
+#PBS -l walltime=24:00:00,mem=64gb,nodes=1:ppn=4
 # #PBS -l walltime=24:00:00,mem=24gb,nodes=1:ppn=24
 # #PBS -l walltime=24:00:00,mem=62gb,nodes=1:ppn=24
 #PBS -N environmental_distance_pred
